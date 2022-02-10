a7e3298e7aa6d769daa278051b64cebca05bb784
NaorNC <81817039+NaorNC@users.noreply.github.com>
2022-02-09 17:18:51 -0800

    Add files via upload

---
+++ nc-codes/main.lua
@@ -0,0 +1,31 @@
+NCCore = nil
+TriggerEvent('NCCore:GetObject', function(obj) NCCore = obj end)
+
+RegisterCommand('usecode', function(source, args, rawCommand)
+    local src = source
+    local xPlayer = NCCore.Functions.GetPlayer(source)
+    local code = table.concat(args, " ")
+
+    exports['ghmattimysql']:execute('SELECT * FROM ncodes WHERE code = @code', {['@code'] = code}, function(result)
+
+        if(result[1] == nil) then
+            return TriggerClientEvent("NCCore:Notify", "The code doesn't exists")
+        end
+
+        exports['ghmattimysql']:execute('DELETE FROM ncodes WHERE code = @code', {['@code'] = code}, function(result) end)
+
+        local type = result[1].type
+        local amount = result[1].amount
+
+        if type == "cash" or type == "bank" then
+            xPlayer.Functions.AddMoney(type, amount)
+            TriggerClientEvent("NCCore:Notify", src, string.format("You Redeemed A Money Token. %s Goes To Your %s", amount, type == "bank" or "Bank Account" and "Wallet"))
+        elseif(type == "item") then
+            xPlayer.Functions.AddItem(amount, 1, nil, nil)
+            TriggerClientEvent("NCCore:Notify", src, string.format("You Redeemed A Item Token. You Got %s", amount))
+        else
+            TriggerClientEvent("NCCore:Notify", src, "Unknown Token Type!, You Will Not Get Anything")
+        end
+        
+    end)
+end)
